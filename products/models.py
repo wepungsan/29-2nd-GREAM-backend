15d4605be64cdcaa374f49b20fac95372f85c311
kangil2 <leki305@gmail.com>
2022-02-16 21:12:15 +0900

    Modify: fixed model

--- products/models.py
+++ products/models.py
@@ -7,7 +7,6 @@ class Product(TimeStamp):
     category       = models.ForeignKey('Category', on_delete = models.CASCADE, related_name = 'product')
     theme          = models.ForeignKey('Theme', on_delete = models.CASCADE, related_name ='product')
     author         = models.ForeignKey('Author', on_delete = models.CASCADE, related_name = 'product')
-    size           = models.ForeignKey('Size', on_delete = models.CASCADE, related_name = 'product')
     release_price  = models.DecimalField(max_digits = 15, decimal_places=2)
     model_number   = models.IntegerField()
     release_date   = models.DateTimeField()
@@ -18,8 +17,16 @@ class Product(TimeStamp):
 class ProductImage(models.Model):
     product    = models.ForeignKey('Product', on_delete = models.CASCADE)
     image_urls = models.URLField()
+    
     class Meta:
         db_table = 'images'
+
+class ProductSize(models.Model):
+    size    = models.ForeignKey('Size', on_delete = models.CASCADE, related_name = 'products_sizes')
+    product = models.ForeignKey('Product', on_delete = models.CASCADE, related_name = 'products_sizes')
+
+    class Meta:
+        db_table = 'products_size'
 
 class Size(models.Model):
     name = models.CharField(max_length = 50)
