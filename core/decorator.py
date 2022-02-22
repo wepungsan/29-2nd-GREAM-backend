aacdb67daf7a695dc6fc66c061567399ebe1f59c
kangil2 <leki305@gmail.com>
2022-02-22 15:25:14 +0900

    [Add] decorator

---
+++ core/decorator.py
@@ -0,0 +1,23 @@
+import jwt
+
+from django.http  import JsonResponse
+from django.conf  import settings
+
+from users.models import User
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization', None)         
+            payload      = jwt.decode(access_token, settings.SECRET_KEY, settings.ALGORITHM)  
+            request.user = User.objects.get(id=payload['user_id'])
+
+        except jwt.exceptions.DecodeError:                                  
+            return JsonResponse({'message' : 'INVALID_TOKEN' }, status=401)
+
+        except User.DoesNotExist:                                        
+            return JsonResponse({'message' : 'INVALID_USER'}, status=401)
+
+        return func(self, request, *args, **kwargs)
+
+    return wrapper
